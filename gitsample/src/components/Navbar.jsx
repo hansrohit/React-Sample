634c5fa5a9b0bdd68211416083f7d824e83f5e8c
Hans Rohit <hansrohit031@gmail.com>
2024-09-17 10:14:23 +0530

    map correction commit 1

--- gitsample/src/components/Navbar.jsx
+++ gitsample/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 const Navbar = (props) => {
+  const users= props.data
+  // console.log(users)
+  // users.map((user)=>{
+  //   console.log(user)
+  // })
   return (
     <>
     
-    <div className="w-full h-[3vh] grad-blue-bg text-white flex justify-center items-center gap-4">
-    {props.data.map(val=>{<p>name:{val.name}, age:{val.age}</p>
-        console.log(`name:${val.name}, age:${val.age}`)})}
+      <ul className='bg-green-400'>
+      {
+        users.map((user,index)=>(
+          <li key={index}> {user.name}</li>
+        ))
+      }
+      </ul>
+    {/* 23456 */}
+    <div className="w-full h-[3vh] grad-blue-bg text-black flex justify-center items-center gap-4">
 
         {/* const ans={props.data.filter(val.age>20=>{console.log(`name:${val.name}, age:${val.age}`)})} */}
     </div>
